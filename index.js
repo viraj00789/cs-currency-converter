8fa2682e42347bb42986c8db3f709e40cb01ba29
MI-571 <viraj.koradia@mindinventory.com>
2025-07-18 15:02:14 +0530

    fix : added the comment

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ dotenv.config();
 const KEY = process.env.API_KEY
 const freecurrencyapi = new Freecurrencyapi(KEY);
 
+// this is the function to convert one curr to another curr. .
 export const myCurrencyConverter = async (fromCurrency, toCurrency, units) => {
   const response = await freecurrencyapi.latest({
     base_currency: fromCurrency,

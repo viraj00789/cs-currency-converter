e508a16ff716d38d5aa100660dca3eec7906f4f9
MI-571 <viraj.koradia@mindinventory.com>
2025-07-18 14:13:16 +0530

    fix : added the gitignore and safe the ENV

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import Freecurrencyapi from "@everapi/freecurrencyapi-js";
+import dotenv from 'dotenv';
+dotenv.config();
 
-const freecurrencyapi = new Freecurrencyapi(
-  "fca_live_n59IVyjV990kBcf2t2z3WjaYAezuaOzCIm4OinlR"
-);
+const KEY = process.env.API_KEY
+const freecurrencyapi = new Freecurrencyapi(KEY);
 
 export const myCurrencyConverter = async (fromCurrency, toCurrency, units) => {
   const response = await freecurrencyapi.latest({
     base_currency: fromCurrency,
     currencies: toCurrency,
   });
-  const result = response?.data[toCurrency] * units;
+  const result = response?.data?.[toCurrency] * units;
   return result;
 };
-
